122ab7d06260445fcd3ceb6fcce6ad14cd0b1fb1
Gitea <gitea@fake.local>
2018-05-25 12:26:30 +0800

    md5

--- src/main/java/com/gamewolf/util/crytography/EncoderUtil.java
+++ src/main/java/com/gamewolf/util/crytography/EncoderUtil.java
@@ -1,6 +1,8 @@
 package com.gamewolf.util.crytography;
 
 import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class EncoderUtil {
@@ -36,9 +38,45 @@ public class EncoderUtil {
 		}
 		
 	}
+	public static String MD5(String str){
+		 byte [] buf = str.getBytes();
+	        MessageDigest md5;
+			try {
+				md5 = MessageDigest.getInstance("MD5");
+				 md5.update(buf);
+			        byte [] tmp = md5.digest();
+			        StringBuilder sb = new StringBuilder();
+			        for (byte b:tmp) {
+			        	String s=Integer.toHexString(b&0xff);
+			        	if(s.length()==1){
+			        		s="0"+s;
+			        	}
+			            sb.append(s);
+			        }
+			        return sb.toString();
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return "";
+
+	}
+	
+	public static String MD5(String str,int length){
+		return MD5(str).substring(0, length);
+	}
+	
+	public static String MD5(String str,int length,boolean isUppercase) {
+		if(isUppercase) {
+			return MD5(str).substring(0, length).toUpperCase();
+		}else {
+			return MD5(str).substring(0, length).toLowerCase();
+		}
+		
+	}
 	
 	public static void main(String args[]) {
-		System.out.print(EncoderUtil.base64Decode("NzU1NjgwMzAwMjk1NA=="));
+		System.out.print(EncoderUtil.MD5("你好",24));
 	}
 
 }
