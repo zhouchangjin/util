0b76229c2204b95a8fd7c801a102b2a92e7e0628
zhouchangjin <iamzhouchangjin@163.com>
2019-11-26 22:08:29 +0800

    读文件

--- src/main/java/com/gamewolf/util/file/FileUtil.java
+++ src/main/java/com/gamewolf/util/file/FileUtil.java
@@ -1,6 +1,10 @@
 package com.gamewolf.util.file;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileUtil {
 	
@@ -10,5 +14,26 @@ public class FileUtil {
 		if(!f.exists()) {
 			f.mkdirs();
 		}
+	}
+
+	public static String readTxtFile(String filePath) {
+		StringBuffer sb=new StringBuffer();
+		File file=new File(filePath);
+		try {
+			
+			
+			BufferedReader br=new BufferedReader(new FileReader(file));
+			String line=null;
+			while((line=br.readLine())!=null) {
+				
+				sb.append(line);
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			return sb.toString();
+		} catch (IOException e) {
+			return sb.toString();
+		}
+		return sb.toString();
 	}
 }
